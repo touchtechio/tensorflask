d1b6d498517051ecd5a9fdd8e5fc378222d00203
Matt Pinner <mpinner@gmail.com>
2018-10-08 14:38:14 -0700

    creation of docker media rest api microservice example

---
+++ ansible/example/media/media.py
@@ -0,0 +1,138 @@
+from datetime import datetime
+# 3rd party modules
+from flask import (
+    make_response,
+    abort
+)
+
+
+def get_timestamp():
+    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
+
+
+# Data to serve with our API
+MEDIA = {
+    "guid_media_999": {
+        "guid": "guid_media_999",
+        "filename": "VID_20180803_201053.mp4",
+        "uri": "https://s3-us-west-2.amazonaws.com/content.touchtech.io/media/VID_20180803_201053.mp4",
+        "timestamp": get_timestamp()
+    },
+    "guid_media_998": {
+        "guid": "guid_media_998",
+        "filename": "spider-robots.MOV",
+        "uri": "https://s3-us-west-2.amazonaws.com/content.touchtech.io/media/spider-robots.MOV",
+        "timestamp": get_timestamp()
+    },
+    "guid_media_997": {
+        "guid": "guid_media_997",
+        "filename": "guid_media_997.mp4",
+        "uri": "nfs://MEDIA/guid_media_997.mp4",
+        "timestamp": get_timestamp()
+    },
+    "guid_media_996": {
+        "guid": "guid_media_996",
+        "filename": "7C2A1862-roto-before.mov",
+        "uri": "https://s3-us-west-2.amazonaws.com/content.touchtech.io/media/7C2A1862-roto-before.mov",
+        "timestamp": get_timestamp()
+    }
+}
+
+
+# Create a handler for our read (GET) media list
+def read_all():
+    """
+    This function responds to a request for /api/media
+    with the complete lists of media
+
+    :return:        sorted list of media
+    """
+    # Create the list of media from our data
+    return [MEDIA[key] for key in sorted(MEDIA.keys())]
+
+
+# Create a handler for our read (GET) media
+def read_one(guid):
+    """
+    This function responds to a request for /api/media/{guid}
+
+    :param guid:    guid of media record to find
+    :return:        media record
+    """
+
+    # does the media exist
+    if guid in MEDIA:
+        media = MEDIA.get(guid)
+
+    # otherwise, nope, not found error
+    else:
+        abort(404, 'Media with given guid {guid} not found'.format(guid=guid))
+
+    return media
+
+
+# Create a handler for our read (GET) media
+def create(media):
+    """
+    This function responds to a request for /api/media/{guid}
+
+    :param media:   media record to create
+    :return:        201 on success, 406 on media record already exists
+    """
+
+    guid = media.get('guid', None)
+    filename = media.get('filename', None)
+    uri = media.get('uri', None)
+
+    # if the the media does not exist, CREATE
+    if guid not in MEDIA and guid is not None:
+        MEDIA[guid] = {
+            'guid': guid,
+            'filename': filename,
+            'uri': uri
+        }
+
+        return make_response('{guid} successfully create for {filename}'.format(guid=guid, filename=filename), 201)
+
+    # otherwise, it exists, so error
+    else:
+        abort(406, 'media already exists with given guid {guid}'.format(guid=guid))
+
+
+def update(guid, media):
+    """
+    This function updates an existing media in the media structure
+    :param guid:   guid of media to update in the media structure
+    :param media:  media to update
+    :return:        updated media structure
+    """
+    # Does the media exist in media list?
+    if guid in MEDIA:
+        MEDIA[guid]['filename'] = media.get('filename')
+        MEDIA[guid]['uri'] = media.get('uri')
+        MEDIA[guid]['timestamp'] = get_timestamp()
+
+        return MEDIA[guid]
+
+    # otherwise, nope, that's an error
+    else:
+        abort(404, 'Media with guid {guid} not found'.format(
+            guid=guid))
+
+
+def delete(guid):
+    """
+    This function deletes a media from the media structure
+    :param media:   guid of media to delete
+    :return:        200 on successful delete, 404 if not found
+    """
+    # Does the media to delete exist?
+    if guid in MEDIA:
+        del MEDIA[guid]
+        return make_response('{guid} successfully deleted'.format(
+            guid=guid), 200)
+
+    # Otherwise, nope, media to delete not found
+    else:
+        abort(404, 'Media with guid  {guid} not found'.format(
+            guid=guid))
---
+++ ansible/example/media/static/js/home.js
@@ -0,0 +1,262 @@
+/*
+ * JavaScript file for the application to demonstrate
+ * using the API
+ */
+
+// Create the namespace instance
+let ns = {};
+
+// Create the model instance
+ns.model = (function() {
+    'use strict';
+
+    let $event_pump = $('body');
+
+    // Return the API
+    return {
+        'read': function() {
+            let ajax_options = {
+                type: 'GET',
+                url: 'api/media',
+                accepts: 'application/json',
+                dataType: 'json'
+            };
+            $.ajax(ajax_options)
+            .done(function(data) {
+                $event_pump.trigger('model_read_success', [data]);
+            })
+            .fail(function(xhr, textStatus, errorThrown) {
+                $event_pump.trigger('model_error', [xhr, textStatus, errorThrown]);
+            })
+        },
+        create: function(media) {
+            let ajax_options = {
+                type: 'POST',
+                url: 'api/media',
+                accepts: 'application/json',
+                contentType: 'application/json',
+                dataType: 'json',
+                data: JSON.stringify(media)
+            };
+            $.ajax(ajax_options)
+            .done(function(data) {
+                $event_pump.trigger('model_create_success', [data]);
+            })
+            .fail(function(xhr, textStatus, errorThrown) {
+                $event_pump.trigger('model_error', [xhr, textStatus, errorThrown]);
+            })
+        },
+        update: function(media) {
+            let ajax_options = {
+                type: 'PUT',
+                url: `api/media/${media.guid}`,
+                accepts: 'application/json',
+                contentType: 'application/json',
+                dataType: 'json',
+                data: JSON.stringify(media)
+            };
+            $.ajax(ajax_options)
+            .done(function(data) {
+                $event_pump.trigger('model_update_success', [data]);
+            })
+            .fail(function(xhr, textStatus, errorThrown) {
+                $event_pump.trigger('model_error', [xhr, textStatus, errorThrown]);
+            })
+        },
+        'delete': function(media) {
+            let ajax_options = {
+                type: 'DELETE',
+                url: `api/media/${media}`,
+                accepts: 'application/json',
+                contentType: 'plain/text'
+            };
+            $.ajax(ajax_options)
+            .done(function(data) {
+                $event_pump.trigger('model_delete_success', [data]);
+            })
+            .fail(function(xhr, textStatus, errorThrown) {
+                $event_pump.trigger('model_error', [xhr, textStatus, errorThrown]);
+            })
+        }
+    };
+}());
+
+// Create the view instance
+ns.view = (function() {
+    'use strict';
+
+    let $guid = $('#guid'),
+        $uri = $('#uri'),
+        $filename = $('#filename');
+
+    // return the API
+    return {
+        reset: function() {
+            $guid.val('');
+            $filename.val('');
+            $uri.val('').focus();
+        },
+        update_editor: function(media) {
+            $guid.val(media.guid);
+            $filename.val(media.filename);
+            $uri.val(media.uri).focus();
+        },
+        build_table: function(media) {
+            let rows = ''
+
+            // clear the table
+            $('.media table > tbody').empty();
+
+            // did we get a media array?
+            if (media) {
+                for (let i=0, l=media.length; i < l; i++) {
+                    rows += `<tr data-media-id="${media[i].guid}">
+                        <td class="guid">${media[i].guid}</td>
+                        <td class="filename"><a href="${media[i].uri}">${media[i].filename}</a></td>
+                        <td class="uri">${media[i].uri}</td>
+                        <td>${media[i].timestamp}</td>
+                    </tr>`;
+                }
+                $('table > tbody').append(rows);
+            }
+        },
+        error: function(error_msg) {
+            $('.error')
+                .text(error_msg)
+                .css('visibility', 'visible');
+            setTimeout(function() {
+                $('.error').css('visibility', 'hidden');
+            }, 3000)
+        }
+    };
+}());
+
+// Create the controller
+ns.controller = (function(m, v) {
+    'use strict';
+
+    let model = m,
+        view = v,
+        $event_pump = $('body'),
+        $guid = $('#guid'),
+        $uri = $('#uri'),
+        $filename = $('#filename');
+
+    // Get the data from the model after the controller is done initializing
+    setTimeout(function() {
+        model.read();
+    }, 100)
+
+    // Validate input
+    function validate(uri, filename) {
+        return uri !== "" && filename !== "";
+    }
+
+    // Create our event handlers
+    $('#create').click(function(e) {
+        let uri = $uri.val(),
+            guid = $guid.val(),
+            filename = $filename.val();
+
+        e.preventDefault();
+
+        if (validate(uri, filename)) {
+            model.create({
+                'guid': guid,
+                'uri': uri,
+                'filename': filename
+            })
+        } else {
+            alert('Problem with media input');
+        }
+    });
+
+    $('#update').click(function(e) {
+        let guid = $guid.val(),
+            uri = $uri.val(),
+            filename = $filename.val();
+
+        e.preventDefault();
+
+        if (validate(uri, filename)) {
+            model.update({
+                guid: guid,
+                uri: uri,
+                filename: filename
+            })
+        } else {
+            alert('Problem with media input');
+        }
+        e.preventDefault();
+    });
+
+    $('#delete').click(function(e) {
+        let guid = $guid.val();
+
+        e.preventDefault();
+
+        if (validate('placeholder', filename)) {
+            model.delete(guid)
+        } else {
+            alert('Problem with uri filename input');
+        }
+        e.preventDefault();
+    });
+
+    $('#reset').click(function() {
+        view.reset();
+    })
+
+    $('table > tbody').on('dblclick', 'tr', function(e) {
+        let $target = $(e.target),
+            guid,
+            uri,
+            filename;
+
+        guid = $target
+            .parent()
+            .attr('data-media-id');
+
+        uri = $target
+            .parent()
+            .find('td.uri')
+            .text();
+
+        filename = $target
+            .parent()
+            .find('td.filename')
+            .text();
+
+        view.update_editor({
+            guid: guid,
+            uri: uri,
+            filename: filename,
+        });
+    });
+
+    // Handle the model events
+    $event_pump.on('model_read_success', function(e, data) {
+        view.build_table(data);
+        view.reset();
+    });
+
+    $event_pump.on('model_create_success', function(e, data) {
+        model.read();
+    });
+
+    $event_pump.on('model_update_success', function(e, data) {
+        model.read();
+    });
+
+    $event_pump.on('model_delete_success', function(e, data) {
+        model.read();
+    });
+
+    $event_pump.on('model_error', function(e, xhr, textStatus, errorThrown) {
+        let error_msg = textStatus + ': ' + errorThrown + ' - ' + xhr.responseJSON.detail;
+        view.error(error_msg);
+        console.log(error_msg);
+    })
+}(ns.model, ns.view));
+
+
