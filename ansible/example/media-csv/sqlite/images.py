2da6e20e02aed0ab66e9bda66a711ed2e920e332
Matt Pinner <mpinner@gmail.com>
2019-02-28 18:26:55 -0800

    added tagging support and example client

--- ansible/example/media-csv/sqlite/images.py
+++ ansible/example/media-csv/sqlite/images.py
@@ -5,8 +5,8 @@ import pandas as pd
 
 class Images(object):
     """sqlite3 database class that holds testers jobs"""
-    __DB_LOCATION = "laika_images.sqlite"
-    __LAIKA_IMAGE_CSV = "../data/data/laika-images.csv"
+    __DB_LOCATION = "Laika-Kubo.sqlite"
+    __LAIKA_IMAGE_CSV = "../data/data/Laika-Kubo.csv"
 
     def __init__(self, db_location=None):
         """Initialize db class variables"""
@@ -51,22 +51,61 @@ class Images(object):
 
         with open(path) as csvfile:
             reader = csv.DictReader(csvfile)
+            index = 0
             for row in reader:
-                self.cur.execute(
-                    "INSERT INTO IMAGES (ID,FILENAME,PATH,TAGS) VALUES (" + str(reader.line_num) + ", '" + row[
-                        'ImageFilename'] + "', '" + row['ImageDirectory'] + "', 'original')");
-                if (reader.line_num % 100 == 0):
-                    print(reader.line_num, ':', row['ImageFilename'])
 
+                # index could be line number but our csv doesn't quite lineup
+                # index = reader.line_num
+                # we'll increment index at the end or figure out how to make this db autoincrement
+
+                # insert a row
+                self.cur.execute(
+                    "INSERT INTO IMAGES (ID,FILENAME,PATH,TAGS) VALUES (" + str(index) + ", '" + row[
+                        'ImageFilename'] + "', '" + row['ImageDirectory'] + "', 'original')");
+
+                # print once every hundred rows so we know something is happening
+                if (index % 100 == 0):
+                    print(index, ':', row['ImageFilename'])
+
+                # commit each insertion
                 self.commit()
+                index += 1
+
+            print(index, ' rows inserted!')
 
     def execute(self, new_data):
         """execute a row of data to current cursor"""
         self.cur.execute(new_data)
 
+    def get_by_sql(self, sql):
+        """execute a row of data to current cursor"""
+        image = pd.read_sql_query(sql, self.__db_connection)
+        return image
+
+    def get_by_name(self, filename):
+        """execute a row of data to current cursor"""
+        image = self.get_by_sql("SELECT * FROM IMAGES where FILENAME = '" + filename + "'")
+        return image
+
+    def get_by_index(self, index):
+        """execute a row of data to current cursor"""
+        image = self.get_by_sql("SELECT * FROM IMAGES where ID = " + str(index))
+        return image
+
+    def get_all_by_tag(self, tag):
+        """execute a row of data to current cursor"""
+        sql = "SELECT * FROM IMAGES where TAGS LIKE '%" + tag + "%'"
+        print(sql)
+        image = self.get_by_sql(sql)
+        return image
+
     def get_all(self):
-        images_table = pd.read_sql_query("SELECT * FROM IMAGES", self.__db_connection)
+        images_table = self.get_by_sql("SELECT * FROM IMAGES")
         return images_table
+
+    # #not quite working
+    # def update(self, panda):
+    #     panda.to_sql("IMAGES", self.__db_connection, if_exists="replace")
 
     def commit(self):
         """commit changes to database"""
